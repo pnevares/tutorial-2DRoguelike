20487768007c95b7ab23a35b9c3b6bfee56d4602
Pablo Nevares <pablo@nevares.com>
2017-12-03 19:55:36 -0700

    part 12

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Player : MovingObject {
@@ -9,6 +10,7 @@ public class Player : MovingObject {
 	public int pointsPerFood = 10;
 	public int pointsPerSoda = 20;
 	public float restartLevelDelay = 1f;
+	public Text foodText;
 
 	private Animator animator;
 	private int food;
@@ -18,6 +20,8 @@ public class Player : MovingObject {
 		animator = GetComponent<Animator> ();
 
 		food = GameManager.instance.playerFoodPoints;
+
+		foodText.text = "Food: " + food;
 
 		base.Start ();
 	}
@@ -46,6 +50,7 @@ public class Player : MovingObject {
 
 	protected override void AttemptMove<T>(int xDir, int yDir) {
 		food--;
+		foodText.text = "Food: " + food;
 
 		base.AttemptMove<T> (xDir, yDir);
 
@@ -62,9 +67,11 @@ public class Player : MovingObject {
 			enabled = false;
 		} else if (other.tag == "Food") {
 			food += pointsPerFood;
+			foodText.text = "+" + pointsPerFood + " Food: " + food;
 			other.gameObject.SetActive(false);
 		} else if (other.tag == "Soda") {
 			food += pointsPerSoda;
+			foodText.text = "+" + pointsPerSoda + " Food: " + food;
 			other.gameObject.SetActive(false);
 		}
 	}
@@ -82,6 +89,7 @@ public class Player : MovingObject {
 	public void LoseFood(int loss) {
 		animator.SetTrigger ("playerHit");
 		food -= loss;
+		foodText.text = "-" + loss + " Food: " + food;
 		CheckIfGameOver ();
 	}
 
