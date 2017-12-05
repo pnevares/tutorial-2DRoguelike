f2ef48ad7f166d7da7a32ffd3ded6e3948b04b6f
Pablo Nevares <pablo@nevares.com>
2017-12-04 23:16:45 -0700

    updated enemy AI, testing gitignore

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -35,7 +35,9 @@ public class Enemy : MovingObject {
 		int xDir = 0;
 		int yDir = 0;
 
-		if (Mathf.Abs (target.position.x - transform.position.x) < float.Epsilon) {
+		// randomly move closer on x- or y-axis (unless y position matches)
+		// original was predictable, prioritizing x- over y-axis
+		if(Random.Range(0, 2) == 0 && target.position.y != transform.position.y) {
 			yDir = target.position.y > transform.position.y ? 1 : -1;
 		} else {
 			xDir = target.position.x > transform.position.x ? 1 : -1;
